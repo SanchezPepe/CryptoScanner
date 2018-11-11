e60ffc086f1f289bcca670a6f387345491776b11
sanchezpepe <sanchezpepe97@gmail.com>
2018-11-10 21:42:59 -0600

    Multiple ChangeDics supported

--- CryptoTrack.py
+++ CryptoTrack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
+# In[1]:
 
 
 import requests
@@ -16,31 +16,39 @@ import time
 from IPython.display import clear_output
 
 
-# In[6]:
+# In[2]:
 
 
 def clear():
-    os.system( 'cls' )
-
-
-# In[7]:
-
-
-def reqAPI():
-    r = requests.get('https://www.cryptopia.co.nz/api/GetMarkets/BTC')
-    coins = json.loads(r.text)['Data']
-    if type(coins) != 'NoneType':
-        return coins #lista de diccionarios 
+    os.system('cls')
 
 
 # In[3]:
 
 
+def reqAPI():
+    try:
+        r = requests.get('https://www.cryptopia.co.nz/api/GetMarkets/BTC')
+        coins = json.loads(r.text)['Data']
+        if coins is not None:
+            return coins #lista de diccionarios 
+    except ValueError:
+        print('Error al hacer el request, intentando nuevamente')
+        reqAPI()
+
+
+# In[4]:
+
+
 def refreshData():
     #API REQUEST
     lenDic, coc, last, volC = 0, 0, 0, 0
+    ask, bid, spread = 0, 0, 0
     old, recent = 0, 0
     coins = reqAPI()
+    while coins is None:
+        coins = reqAPI()
+        print(type(coins))
     for y in coins:
         subDic = {}
         changeDic = {}
@@ -49,7 +57,7 @@ def refreshData():
             lenDic = len(dic[name])
         else:
             dic[name] = {}
-        time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+        ts = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
         if y['BidPrice'] != 0:
             spread = ((y['AskPrice']/y['BidPrice'])-1)*100
             spread = round(spread, 2)
@@ -72,57 +80,66 @@ def refreshData():
                 volC = 0
             else:
                 volC = 1
-            if coc > 0.05 or volC > 0.05: #Si el cambio en precios o en volumen es mayor al 5%
-                subDic[time] = changeDic
-                toBuy[name] = subDic
             changeDic['coc'] = coc
             changeDic['volC'] = volC
+            if coc > 0.0 and volC > 0.0: #Si el cambio en precios o en volumen es mayor al 5%
+                if not name in toBuy:
+                    toBuy[name] = {}
+                toBuy[name][ts] = changeDic
         if lenDic == 5: 
             old = [*dic[name]][0] #Registro más viejo
             dic[name].pop(old)
         #Inserta el nuevo
-        dic[name][time] = changeDic
-
-
-# In[10]:
-
-
-#Variables
-#Diccionarios con los datos
-toBuy = {}
-ask, bid, spread = 0, 0, 0
-dic, subDic, changeDic = {}, {}, {}
-pp = pprint.PrettyPrinter(indent=1)
-i = 0
-n = 100
-suma = 0
-
-while i < n:
-    #clear_output()
-    start = time.time()
-    refreshData()
-    end = time.time()
-    suma += end-start
-    clear()
-    pp.pprint(toBuy)
-    i += 1
-    sleep(1.5)
-
-#Tiempo promedio por respuesta
-print('Tiempo promedio de las ejecuciones: ', suma/n)
+        dic[name][ts] = changeDic
 
 
 # In[5]:
 
 
+#Variables
+#Diccionarios con los datos
+def analiza(n):
+    pp = pprint.PrettyPrinter(indent=1)
+    i = 0
+    suma = 0
+    toBuySize = 0
+    while i < n:
+        start = time.time()
+        refreshData()
+        end = time.time()
+        suma += end-start
+        if toBuySize < len(toBuy): #Si se agregó un nvo item a toBuy
+            clear()
+            #clear_output()
+            pp.pprint(toBuy)
+        i += 1
+        toBuySize = len(toBuy)
+        sleep(1.5)
+        
+    #Tiempo promedio por respuesta
+    print('Tiempo promedio de las ejecuciones: ', suma/n)
+
+
+# In[15]:
+
+
 #Coin Analisis
 #pp.pprint(dic)
 #pp.pprint(dic['$$$/BTC'])
-""""
-coin = 'BTB/BTC'
+dic, toBuy, changeDic = {}, {}, {}
+n = input('Ingresar número de pruebas a ejecutar ')
+if type(n) is str:
+    n = int(n)
+    if n > 0:
+        analiza(n)
+else:
+    print('Ingresa un número mayor a 0')
 
-pp.pprint(dic[coin])
-"""
+
+# In[16]:
+
+
+
 
 
 # In[ ]:
