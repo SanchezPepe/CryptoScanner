ece92510d00cd01d952fe0c59255f5293ae98ab7
sanchezpepe <sanchezpepe97@gmail.com>
2018-12-26 22:48:41 -0600

    mastr

--- CryptoTrack.py
+++ CryptoTrack.py
@@ -14,10 +14,12 @@ from datetime import datetime
 from time import sleep
 import time
 from IPython.display import clear_output
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 
 # In[2]:
-pp = pprint.PrettyPrinter(indent=1)
 
 
 def clear():
@@ -97,6 +99,15 @@ def refreshData():
                 if not name in toBuy:
                     toBuy[name] = {}
                 toBuy[name][ts] = changeDic
+                if name not in top: #Si no existe se agrega el primero
+                    top[name] = [coc, volC, bVol]
+                else: #Compara c/u para buscar el máximo
+                    if top[name][0] < coc:
+                        top[name][0] = coc
+                    if top[name][1] < volC:
+                        top[name][1] = volC
+                    if top[name][2] < bVol:
+                        top[name][2] = bVol
         if lenDic == 5: 
             old = [*dic[name]][0] #Registro más viejo
             dic[name].pop(old)
@@ -119,9 +130,10 @@ def analiza(n):
         end = time.time()
         suma += end-start
         if toBuySize < len(toBuy): #Si se agregó un nvo item a toBuy
-            clear()
-            #clear_output()
-            pp.pprint(toBuy)
+            #clear()
+            clear_output()
+            #plotChanges()
+            #pp.pprint(toBuy)
         i += 1
         toBuySize = len(toBuy)
         sleep(1.5)
@@ -130,13 +142,15 @@ def analiza(n):
     print('Tiempo promedio de las ejecuciones: ', suma/n)
 
 
-# In[15]:
+# In[6]:
 
 
 #Coin Analisis
 #pp.pprint(dic)
 #pp.pprint(dic['$$$/BTC'])
+pp = pprint.PrettyPrinter(indent=4)
 dic, toBuy, changeDic = {}, {}, {}
+top = {}
 n = input('Ingresar número de pruebas a ejecutar ')
 if type(n) is str:
     n = int(n)
@@ -146,10 +160,58 @@ else:
     print('Ingresa un número mayor a 0')
 
 
-# In[16]:
+# In[15]:
 
 
+def autolabel(rects, xpos='center'):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
 
+    *xpos* indicates which side to place the text w.r.t. the center of
+    the bar. It can be one of the following {'center', 'right', 'left'}.
+    """
+
+    xpos = xpos.lower()  # normalize the case of the parameter
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,'{}'.format(height), ha=ha[xpos], va='bottom')
+
+
+# In[47]:
+
+
+chng, volch, bvolc = [], [], []
+
+for i in top:
+    chng.append(round(top[i][0]*100, 0))
+    volch.append(round(top[i][1]*100, 0))
+    bvolc.append(round(top[i][2]*100, 0))    
+    
+ind = np.arange(len(chng))  # the x locations for the groups
+width = 0.25  # the width of the bars
+    
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind - width, chng, width, color='SkyBlue', label='Change')
+rects2 = ax.bar(ind, volch, width, color='IndianRed', label='Vol Change')
+rects3 = ax.bar(ind + width, bvolc, width, color='Green', label='Buy Vol Change')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Change %')
+ax.set_title('Cambio en el mercado de cryptos')
+
+ax.set_xticks(ind)
+ax.set_xticklabels(list(top.keys()))
+ax.legend()
+
+autolabel(rects1, "center")
+autolabel(rects2, "center")
+autolabel(rects3, "center")
+
+plt.grid(True, linewidth=0.25)
+plt.show()
 
 
 # In[ ]:
